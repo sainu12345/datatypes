85165e66f0779e0ea6ba4b98da44b62dafb6cdda
Sainul Abiheen P <sainulabidheenpsainu@gmail.com>
2024-11-05 20:15:38 +0530

    Add files via upload

---
+++ datatypes.dart
@@ -0,0 +1,23 @@
+void main(){
+  var myname=name();
+  var myage=age();
+  var myaddress=address();
+  var myspace=space();
+  print(myname);
+  print(myage);
+  print(myaddress);
+  print(myspace);
+  
+}
+String name(){
+  return "string=ascd";
+}
+String age(){
+  return "int=1,2,3,4";
+}
+String address(){
+  return "double=2.5,3.6,7.4";
+}
+String space(){
+  return "bool=true,false" ;
+}
